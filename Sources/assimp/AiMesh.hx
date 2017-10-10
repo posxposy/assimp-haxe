adfec8252bc821b886fb5d926d9a64fcc69900a8
Dmitry Hryppa <dmitry.hryppa@gmail.com>
2017-10-10 12:38:47 +0300

    More fields for AiMesh

--- Sources/assimp/AiMesh.hx
+++ Sources/assimp/AiMesh.hx
@@ -18,6 +18,9 @@ extern class AiMesh
     @:native("mMaterialIndex") public var materialIndex:Int;
     
     @:native("mVertices") public var vertices:RawPointer<AiVector3D>;
+    @:native("mNormals") public var normals:RawPointer<AiVector3D>;
     @:native("mFaces") public var faces:RawPointer<AiFace>;
     @:native("mTextureCoords") public var textureCoords:RawPointer<RawPointer<AiVector3D>>;
+    @:native("mTangents") public var tangents:RawPointer<AiVector3D>;
+    @:native("mBitangents") public var bitangents:RawPointer<AiVector3D>;
 }
